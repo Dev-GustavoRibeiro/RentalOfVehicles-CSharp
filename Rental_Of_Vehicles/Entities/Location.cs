03d8a460b952d15545f4cd4e9cedb59495d8c9a6
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2019-12-17 20:21:45 -0300

    Finished

---
+++ Rental_Of_Vehicles/Entities/Location.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Rental_Of_Vehicles
+{
+    class Location
+    {
+        public string CarModel { get; set; }
+        public DateTime Initial { get; set; }
+        public DateTime Last { get; set; }
+        public double ValuePerHour { get; set; }
+        public double DailyValue { get; set; }
+
+        public Location(string carModel, DateTime initial, DateTime last, double valuePerHour, double dailyValue)
+        {
+            CarModel = carModel;
+            Initial = initial;
+            Last = last;
+            ValuePerHour = valuePerHour;
+            DailyValue = dailyValue;
+        }
+
+        public TimeSpan LeaseTime()
+        {
+            TimeSpan time = Last.Subtract(Initial);
+            return time;
+        }
+
+        public double LeaseValue()
+        {
+            TimeSpan time = LeaseTime();
+            
+            if ((time.Days == 0)&&(time.Hours <= 12))
+            {
+                int rounding = 0;
+                if (time.Minutes > 0)
+                {
+                    rounding = 1;
+                }
+                return (time.Hours + rounding) * ValuePerHour;
+            }
+            else
+            {
+                int rounding = 0;
+                if (time.Hours > 0)
+                {
+                    rounding = 1;
+                }
+                return (time.Days + rounding) * DailyValue;  
+            }
+        }
+
+        public double Tax()
+        {
+            if (LeaseValue() <= 100.0)
+            {
+                return LeaseValue() * 0.2;
+            }
+            else
+            {
+                return LeaseValue() * 0.15;
+            }
+        }
+
+        public double Total()
+        {
+            return LeaseValue() + Tax();
+        }
+    }
+}
---
+++ Rental_Of_Vehicles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rental_Of_Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string car = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyy hh:mm): ");
+            DateTime pickup = DateTime.Parse(Console.ReadLine());
+            Console.Write("Return (dd/MM/yyy hh:mm): ");
+            DateTime returned = DateTime.Parse(Console.ReadLine());
+            Console.Write("Enter price per hour: ");
+            double pricehour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double priceday = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Location location = new Location(car, pickup, returned, pricehour, priceday);
+            
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine("Basic payment: " + location.LeaseValue().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Tax: " + location.Tax().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Total payment: " + location.Total().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
